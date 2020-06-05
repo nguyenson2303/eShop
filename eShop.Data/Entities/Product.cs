9512c2f1b00d423e76ef9e5a9d7b49ab55536c9e
Son Nguyen <nguyenson2303@gmail.com>
2020-06-05 14:52:24 +0700

    Add new project vol 07

---
+++ eShop.Data/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace eShop.Data.Entities
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string SeoAlias { get; set; }
+    }
+}
---
+++ eShop.Data/EntityFramework/EShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using eShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Data.EntityFramework
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
