5d767defac31d9ce1c7e7dcec5a3ee7501378302
Son Nguyen <nguyenson2303@gmail.com>
2020-06-10 17:34:15 +0700

    Create application layer structure

---
+++ eShop.Application/Catalog/Products/Dtos/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Application.Catalog.Products.Dtos
+{
+    public class ProductViewModel
+    {
+
+    }
+}
---
+++ eShop.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Application.Catalog.Products.Dtos;
+using eShop.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        PageViewModel<ProductViewModel> GetAllByCAtegoryId(int categoryId, int pageIndex, int pageSize);
+    }
+}
